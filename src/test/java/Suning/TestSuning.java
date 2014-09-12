85730a3b66fc2e11d4bf6071adb9d04fdc652685
AlwaysTerry <admin123>
2014-09-12 16:57:24 +0800

    sd

--- src/test/java/Suning/TestSuning.java
+++ src/test/java/Suning/TestSuning.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -28,15 +29,21 @@ public class TestSuning {
 	        capabilities.setCapability("appActivity", "com.suning.mobile.ebuy.ui.frame.SuningTabActivity");
 	        
 	        driver = new RemoteWebDriver(new URL("http://172.28.57.49:4723/wd/hub"), capabilities);
-	        driver.manage().timeouts().implicitlyWait(10000, TimeUnit.SECONDS);
+	        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	    }
 
 
 	    @Test
-	    public void testCalculator(){
+	    public void orderMobile(){
 	    	
 	    	driver.findElement(By.xpath("//android.widget.RelativeLayout[3]")).click();
-	    	driver.findElement(By.xpath("//android.widget.TextView[text()='手机/数码/配件']")).click();
+	    	driver.findElement(By.xpath("//android.widget.TextView[@text='手机/数码/配件']")).click();
+	    	driver.findElement(By.xpath("//android.widget.TextView[@text='华为']")).click();
+	    	
+	    	WebElement we = driver.findElement(By.xpath("//android.widget.TextView[@text='抱歉，没有找到符合条件的商品!']"));
+	    	if(we.isDisplayed()){
+	    		driver.findElement(By.xpath("//android.widget.LinearLayout//android.widget.ImageView[last()]")).click();
+	    	}
     }
 
 
